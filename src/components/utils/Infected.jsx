c2428f6263d33701d060b1c6909839456891545c
Markjonel02 <rellesmark43@gmail.com>
2025-03-05 11:22:08 +0800

    complete the json file

--- src/components/utils/Infected.jsx
+++ src/components/utils/Infected.jsx
@@ -1,4 +1,75 @@
+import runner from "../../assets/img/Characters_img/Infected/The-Last-Of-Us-Background-Isolated-PNG.png";
+import stalker from "../../assets/img/Characters_img/Infected/stalker.jpg";
+import clicker from "../../assets/img/Characters_img/Infected/Clicker.jpg";
+import shamblers from "../../assets/img/Characters_img/Infected/shamblers.png";
+import bloaters from "../../assets/img/Characters_img/Infected/Bloater.png";
+import ratking from "../../assets/img/Characters_img/Infected/rat king.png";
 
 export const Infected = [
-   infecties_name:""
-]
+  {
+    id: 1,
+    image: runner,
+    Infectedname: "Runners",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+  {
+    id: 2,
+    image: runner,
+    Infectedname: "Stalker",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+  {
+    id: 3,
+    image: runner,
+    Infectedname: "Clicker",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+  {
+    id: 4,
+    image: runner,
+    Infectedname: "Shamblers",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+  {
+    id: 5,
+    image: runner,
+    Infectedname: "Bloaters",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+  {
+    id: 6,
+    image: runner,
+    Infectedname: "Rat king",
+    desc: "The infected are humans who have been infected by the Cordyceps brain infection and subsequently mutated into horrific new forms. The resulting creatures are aggressive, and will attack any uninfected human and animals on sight.[2]The infected are one of the main enemy types in The Last of Us, Left Behind, and The Last of Us Part II, the other being non-infected human enemies.",
+    History: {
+      GroupType: "Infected",
+      Location: "World Wide",
+    },
+    enemies: "Survivor",
+  },
+];
