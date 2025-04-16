cf4d0040bcde40072ba83895b4c4ade216d4ed29
Markjonel02 <rellesmark43@gmail.com>
2025-04-16 15:32:16 +0800

    added more

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,14 @@ import { lazy, Suspense, memo, useEffect } from "react";
 import { Box, Text } from "@chakra-ui/react";
 import BacktoTop from "./components/BacktoTop";
 import { Routes, Route, Outlet } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 const Navigation = lazy(() => import("./components/Navigation"));
-
 const Home = lazy(() => import("./components/Home"));
+const Faq = lazy(() => import("./components/Faqs"));
 const Footer = lazy(() => import("./components/Footer"));
 const Pricing = lazy(() => import("./routes/Pricing"));
 const Nopage = lazy(() => import("./routes/Horror404"));
 import AOS from "aos";
-import "aos/dist/aos.css"; // Import AOS styles
-
-export const RefreshTopRoutes = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
+import "aos/dist/aos.css"; // Import AOS styles const
 
 const LoadingPlaceholder = () => (
   <Box className="loading-container">
@@ -43,29 +32,22 @@ const LoadingPlaceholder = () => (
     </Box>
   </Box>
 );
-
 const Layout = () => (
   <>
     <Navigation />
-    <Outlet />
     <Suspense fallback={<LoadingPlaceholder />}>
-
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="*" element={<Nopage />} />
+        <Route path="pricing" element={<Pricing />} />
       </Routes>
     </Suspense>
-    <Footer />
-    <BacktoTop />
+    <Footer /> <BacktoTop />
   </>
 );
+
 const App = () => {
   useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration in milliseconds
-      easing: "ease-in-out", // Animation easing styleS
-    });
+    AOS.init({ duration: 1000, easing: "ease-in-out" });
   }, []);
   return (
     <Suspense fallback={<LoadingPlaceholder />}>
@@ -76,5 +58,4 @@ const App = () => {
     </Suspense>
   );
 };
-
 export default memo(App);
